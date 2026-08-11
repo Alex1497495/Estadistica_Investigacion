119d65c2504de97b184858973fabbd788c161aae
Alex Solis <alexandro.soliis@gamil.com>
2026-08-11 14:08:38 -0600

    Update

---
+++ Scripts/Primer_Script_proyectoR.R
@@ -0,0 +1,15 @@
+# Alejandro Solis Sanchez
+# 1497495
+# 11/08/2026
+
+
+# Ingresar Datos -----
+
+altura <- 12
+12 + altura
+altura * altura
+altura + dbh
+# Conjunto de datos
+
+dbh <- c(12, 10, 11.5, 13.2, 18.5)
+# Estimar medias
